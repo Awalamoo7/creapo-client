962311ee7e645e513a629e07c21593310179f4be
Awal Amoo <awalamoo7@gmail.com>
2023-06-21 21:55:01 +0100

    material ui

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
+import Container from "@mui/material/Container";
 
 const Item = styled(Paper)(({ theme }) => ({
 	backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -13,21 +14,27 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Home() {
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<Grid container spacing={2}>
-				<Grid xs={8}>
-					<Item>xs=8</Item>
+		<Container component="main" maxWidth="xl">
+			<Box
+				sx={{
+					marginLeft: 0,
+				}}
+			>
+				<Grid container spacing={2}>
+					<Grid xs={8}>
+						<Item>xs=8</Item>
+					</Grid>
+					<Grid xs={4}>
+						<Item>xs=4</Item>
+					</Grid>
+					<Grid xs={4}>
+						<Item>xs=4</Item>
+					</Grid>
+					<Grid xs={8}>
+						<Item>xs=8</Item>
+					</Grid>
 				</Grid>
-				<Grid xs={4}>
-					<Item>xs=4</Item>
-				</Grid>
-				<Grid xs={4}>
-					<Item>xs=4</Item>
-				</Grid>
-				<Grid xs={8}>
-					<Item>xs=8</Item>
-				</Grid>
-			</Grid>
-		</Box>
+			</Box>
+		</Container>
 	);
 }
