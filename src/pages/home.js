80ef6414467f458a8b186ede974fce6c5e826c48
Awal Amoo <awalamoo7@gmail.com>
2023-06-22 02:40:13 +0100

    adjust hoomepage layout

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,4 @@
 import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 import Container from "@mui/material/Container";
@@ -14,27 +13,21 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Home() {
 	return (
-		<Container component="main" maxWidth="xl">
-			<Box
-				sx={{
-					marginLeft: 0,
-				}}
-			>
-				<Grid container spacing={2}>
-					<Grid xs={8}>
-						<Item>xs=8</Item>
-					</Grid>
-					<Grid xs={4}>
-						<Item>xs=4</Item>
-					</Grid>
-					<Grid xs={4}>
-						<Item>xs=4</Item>
-					</Grid>
-					<Grid xs={8}>
-						<Item>xs=8</Item>
-					</Grid>
+		<Container>
+			<Grid container spacing={2}>
+				<Grid xs={8}>
+					<Item>xs=8</Item>
 				</Grid>
-			</Box>
+				<Grid xs={4}>
+					<Item>xs=4</Item>
+				</Grid>
+				<Grid xs={4}>
+					<Item>xs=4</Item>
+				</Grid>
+				<Grid xs={8}>
+					<Item>xs=8</Item>
+				</Grid>
+			</Grid>
 		</Container>
 	);
 }
